aa298e1a19cf6ff25c23e1683f1bff2e5687529a
Amonov Hurshed <amonov@wimark.com>
2024-01-27 01:12:48 +0300

    roach: fugitive: Add new shortcuts

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -27,3 +27,11 @@ autocmd("BufWinEnter", {
         vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
     end,
 })
+vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "gl", function()
+    vim.cmd.Git('log --oneline -10')
+end, {})
+vim.keymap.set("n", "gll", function()
+    vim.cmd.Git('log --oneline')
+end, {})
