f4072664c63dda2fb6272eef8970ba7ddb2072a3
Amonov Hurshed <amonov@wimark.com>
2024-01-31 15:00:58 +0300

    roach: packer.nvim: Gets installed by itself

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -1,7 +1,16 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        local repo = 'https://github.com/wbthomason/packer.nvim'
+        fn.system({'git', 'clone', '--depth', '1', repo, install_path})
+        vim.cmd.packadd('packer.nvim')
+        return true
+    end
+    return false
+end
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
@@ -74,5 +83,10 @@ return require('packer').startup(function(use)
   use("eandrju/cellular-automaton.nvim")
   use("laytan/cloak.nvim")
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if ensure_packer() then
+      require('packer').sync()
+  end
 end)
 
--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = "n:blinkon0"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
