389a509fb575680adedb218c8317b0b7edf7bdd6
Amonov Hurshed <amonov@wimark.com>
2025-05-29 15:26:26 +0300

    roach: Add missing shortcuts to harpoon

--- lua/theprimeagen/lazy/harpoon.lua
+++ lua/theprimeagen/lazy/harpoon.lua
@@ -33,6 +33,14 @@ return {
         vim.keymap.set("n", "<leader><C-h>", function()
             harpoon:list():replace_at(1)
         end)
-
+        vim.keymap.set("n", "<leader><C-t>", function()
+            harpoon:list():replace_at(2)
+        end)
+        vim.keymap.set("n", "<leader><C-n>", function()
+            harpoon:list():replace_at(3)
+        end)
+        vim.keymap.set("n", "<leader><C-s>", function()
+            harpoon:list():replace_at(4)
+        end)
     end
 }
